ceb17d87d5650d965927ae70c8930bc092eed43e
deiu <andrei@fcns.eu>
2013-09-26 23:52:38 +0300

    Added support for certificates during curl operations.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
+CERTCMD=""
+
+# get target URI
 read -p "Please provide the URI of the target dir (ex: http://example.org/apps/myapp/): " HOST
 if [ "$HOST" = "" ]
 then
     read -p "Please provide the URI of the target dir (ex: http://example.org/apps/myapp/): " HOST
 fi
 
+# get certificate path
+read -p "Please provide the path containing the name of your certificate file, in PEM format (leave blank if none): " CERT
+
 # add trailing slash to target dir if not present
 if [ `echo "$HOST" | grep "[^/]$"` ]; then HOST="$HOST/"; fi
 
-# create the dir structure
+# add cert arguments to the curl command
+if [ "$CERT" != "" ]; then CERTCMD="-E $CERT";fi
+
+# create the dir structure (using MKCOL to increase interoperabilty)
 for dir in `find . -mindepth 1 -type d | sed "s|^\./||"`:
 do
     echo "Creating dir: $dir"
-    curl -X 'MKCOL' $HOST$dir
+    curl -X 'MKCOL' $CERTCMD $HOST$dir
 done
 
 # upload the files now
@@ -23,5 +32,5 @@ do
     if [ ! `echo "$file" | grep "[^:]$"` ]; then file="${file%?}";fi
 
     echo "Uploading file: $HOST$file"
-    curl --upload-file $file $HOST$file
+    curl --upload-file $file $CERTCMD $HOST$file
 done
